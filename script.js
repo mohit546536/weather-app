e697c9519d2bb2501d24fa89315ececba293c563
mohit546536 <mohitgagde72@gmail.com>
2026-04-22 19:49:47 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+async function getWeather() {
+    let city = document.getElementById("city").value;
+
+    let apiKey = "cdbdea2ca54f24d5be80cd362130c9e7";
+    let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+
+    let response = await fetch(url);
+    let data = await response.json();
+
+    let result = document.getElementById("result");
+
+    if (data.cod == 200) {
+        result.innerHTML = `
+            <h2>${data.name}</h2>
+            <p>🌡 Temp: ${data.main.temp}°C</p>
+            <p>☁ Weather: ${data.weather[0].description}</p>
+            <p>💧 Humidity: ${data.main.humidity}%</p>
+        `;
+    } else {
+        result.innerHTML = "❌ City not found";
+    }
+}
